25d8d4286a8c2289d30dddbf4ac31baa5b6a065a
dario290799 <72132972+dario290799@users.noreply.github.com>
2021-11-30 21:25:42 +0100

    Actualizacion

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -8,20 +8,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import com.example.calculadora.Utils.Operacion;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    // Variables
+    private Button btnUno,btnDos,btnTres,btnCuatro,btnCinco,btnSeis,btnSiete,btnOcho,btnNueve,btnCero,
+            btnMr,btnDiezPor,btnEe,btnRand,btnLogDiez,btnC,btnMasMenos,btnPorcentaje,
+            btnDividir,btnMultiplicar,btnSumar,btnRestar,btnIgual,btnCuadrado,btnCubo;
 
-    // Vertical
-    private Button cero,uno,dos,tres,cuatro,cinco,seis,siete,ocho,nueve,borrar,igual,decimal,sumar,restar,multiplicar,dividir,X2,X3;
-    // Horizontal
-    private Button sin,cos,tan,sqrt,log;
-    private String numeroUno = "";
-    private String numeroDos = "";
+    private String numeroUno="";
+    private String numeroDos="";
+    //private String txtResultado="0";
+    private String ultimaTeclaPulsada;
     private Operacion operacion;
-    private TextView textResultado;
+    private TextView textoResulatado;
+
 
 
     @Override
@@ -30,252 +30,230 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         instancias();
         acciones();
-
-        // Cambiar la orientación de la pantalla
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            horizontal();
-        }
-
-        // Constructor de la clase operación
-        operacion = new Operacion(0, 0, null);
+        if (getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE){
+        accionesHorizontal();}
+        operacion=new Operacion(0,0,null);
     }
 
-    private void horizontal() {
-        sin.setOnClickListener(this::onClick);
-        cos.setOnClickListener(this::onClick);
-        tan.setOnClickListener(this::onClick);
-        sqrt.setOnClickListener(this::onClick);
-        log.setOnClickListener(this::onClick);
+    private void accionesHorizontal() {
+        btnMr.setOnClickListener(this);
+        btnDiezPor.setOnClickListener(this::onClick);
+        btnEe.setOnClickListener(this::onClick);
+        btnRand.setOnClickListener(this::onClick);
+        btnLogDiez.setOnClickListener(this::onClick);
+
     }
 
     private void acciones() {
-        cero.setOnClickListener(this::onClick);
-        uno.setOnClickListener(this::onClick);
-        dos.setOnClickListener(this::onClick);
-        tres.setOnClickListener(this::onClick);
-        cuatro.setOnClickListener(this::onClick);
-        cinco.setOnClickListener(this::onClick);
-        seis.setOnClickListener(this::onClick);
-        siete.setOnClickListener(this::onClick);
-        ocho.setOnClickListener(this::onClick);
-        nueve.setOnClickListener(this::onClick);
-        borrar.setOnClickListener(this::onClick);
-        dividir.setOnClickListener(this::onClick);
-        multiplicar.setOnClickListener(this::onClick);
-        restar.setOnClickListener(this::onClick);
-        sumar.setOnClickListener(this::onClick);
-        igual.setOnClickListener(this::onClick);
-        X2.setOnClickListener(this::onClick);
-        X3.setOnClickListener(this::onClick);
+        btnUno.setOnClickListener(this::onClick);
+        btnDos.setOnClickListener(this::onClick);
+        btnTres.setOnClickListener(this::onClick);
+        btnCuatro.setOnClickListener(this::onClick);
+        btnCinco.setOnClickListener(this::onClick);
+        btnSeis.setOnClickListener(this::onClick);
+        btnSiete.setOnClickListener(this::onClick);
+        btnOcho.setOnClickListener(this::onClick);
+        btnNueve.setOnClickListener(this::onClick);
+        btnCero.setOnClickListener(this::onClick);
+        btnC.setOnClickListener(this::onClick);
+        btnDividir.setOnClickListener(this::onClick);
+        btnMultiplicar.setOnClickListener(this::onClick);
+        btnRestar.setOnClickListener(this::onClick);
+        btnSumar.setOnClickListener(this::onClick);
+        btnIgual.setOnClickListener(this::onClick);
+        btnCubo.setOnClickListener(this);
+        btnCuadrado.setOnClickListener(this);
+
+
     }
 
     private void instancias() {
-        cero = findViewById(R.id.cero);
-        uno = findViewById(R.id.uno);
-        dos = findViewById(R.id.dos);
-        tres = findViewById(R.id.tres);
-        cuatro = findViewById(R.id.cuatro);
-        cinco = findViewById(R.id.cinco);
-        seis = findViewById(R.id.seis);
-        siete = findViewById(R.id.siete);
-        ocho = findViewById(R.id.ocho);
-        nueve = findViewById(R.id.nueve);
-        borrar = findViewById(R.id.borrar);
-        sin = findViewById(R.id.sin);
-        cos = findViewById(R.id.cos);
-        sqrt = findViewById(R.id.btn_sqrt);
-        tan = findViewById(R.id.tan);
-        log = findViewById(R.id.btn_log);
-        dividir = findViewById(R.id.dividir);
-        multiplicar = findViewById(R.id.multiplicar);
-        sumar = findViewById(R.id.sumar);
-        restar = findViewById(R.id.restar);
-        igual = findViewById(R.id.igual);
-        textResultado = findViewById(R.id.textResultado);
-        X2 = findViewById(R.id.bX2);
-        X3 = findViewById(R.id.X3);
+        btnCero=findViewById(R.id.btn_cero);
+        btnUno=findViewById(R.id.btn_uno);
+        btnDos=findViewById(R.id.btn_dos);
+        btnTres=findViewById(R.id.btn_tres);
+        btnCuatro=findViewById(R.id.btn_cuatro);
+        btnCinco=findViewById(R.id.btn_cinco);
+        btnSeis=findViewById(R.id.btn_seis);
+        btnSiete=findViewById(R.id.btn_siete);
+        btnOcho=findViewById(R.id.btn_ocho);
+        btnNueve=findViewById(R.id.btn_nueve);
+        btnMr=findViewById(R.id.btn_MR);
+        btnDiezPor=findViewById(R.id.btn_diez_por);
+        btnEe=findViewById(R.id.btn_ee);
+        btnRand=findViewById(R.id.rand);
+        btnLogDiez=findViewById(R.id.btn_log);
+        btnC=findViewById(R.id.btn_C);
+
+        btnDividir=findViewById(R.id.btn_dividir);
+        btnMultiplicar=findViewById(R.id.btn_multiplicar);
+        btnSumar=findViewById(R.id.btn_mas);
+        btnRestar=findViewById(R.id.btn_menos);
+        btnIgual=findViewById(R.id.btn_igual);
+        textoResulatado=findViewById(R.id.txt_resultado);
+        btnCuadrado=findViewById(R.id.cuadrado);
+        btnCubo=findViewById(R.id.btn_cubo);
+
+
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            // Numeros
-            case R.id.cero:
-                textResultado.append(cero.getText());
+        switch (v.getId()){
+
+
+            case R.id.btn_uno:
+                textoResulatado.append(btnUno.getText());
+
+
                 break;
-            case R.id.uno:
-                textResultado.append(uno.getText());
+            case R.id.btn_dos:
+                textoResulatado.append(btnDos.getText());
+
                 break;
-            case R.id.dos:
-                textResultado.append(dos.getText());
+            case R.id.btn_tres:
+                textoResulatado.append(btnTres.getText());
                 break;
-            case R.id.tres:
-                textResultado.append(tres.getText());
+            case R.id.btn_cuatro:
+                textoResulatado.append(btnCuatro.getText());
                 break;
-            case R.id.cuatro:
-                textResultado.append(cuatro.getText());
+            case R.id.btn_cinco:
+                textoResulatado.append(btnCinco.getText());
                 break;
-            case R.id.cinco:
-                textResultado.append(cinco.getText());
+            case R.id.btn_seis:
+                textoResulatado.append(btnSeis.getText());
                 break;
-            case R.id.seis:
-                textResultado.append(seis.getText());
+            case R.id.btn_siete:
+                textoResulatado.append(btnSiete.getText());
                 break;
-            case R.id.siete:
-                textResultado.append(siete.getText());
+            case R.id.btn_ocho:
+                textoResulatado.append(btnOcho.getText());
                 break;
-            case R.id.ocho:
-                textResultado.append(ocho.getText());
-                break;
-            case R.id.nueve:
-                textResultado.append(nueve.getText());
+            case R.id.btn_nueve:
+                textoResulatado.append(btnNueve.getText());
                 break;
 
-            // Operaciones
-            case R.id.sumar:
+            case R.id.btn_cero:
+                textoResulatado.append(btnCero.getText());
+
+                break;
+
+            case R.id.btn_mas:
                 operacion.setOperacion("+");
-                añadirNumero();
+                añadirOperando();
                 break;
-            case R.id.restar:
+            case R.id.btn_menos:
                 operacion.setOperacion("-");
-                añadirNumero();
+                añadirOperando();
+
                 break;
-            case R.id.multiplicar:
+            case R.id.btn_multiplicar:
                 operacion.setOperacion("*");
-                añadirNumero();
+                añadirOperando();
+
                 break;
-            case R.id.dividir:
+            case R.id.btn_dividir:
                 operacion.setOperacion("/");
-                añadirNumero();
-                break;
-            case R.id.bX2:
-                operacion.setOperacion("X2");
-                añadirNumero();
-                hacerCalculo();
-                break;
-            case R.id.X3:
-                operacion.setOperacion("X3");
-                añadirNumero();
-                hacerCalculo();
-                break;
-            case R.id.btn_log:
-                operacion.setOperacion("log");
-                añadirNumero();
-                hacerCalculo();
-                break;
-            case R.id.btn_sqrt:
-                operacion.setOperacion("sqrt");
-                añadirNumero();
-                hacerCalculo();
-                break;
-            case R.id.cos:
-                operacion.setOperacion("cos");
-                añadirNumero();
-                hacerCalculo();
-                break;
-            case R.id.sin:
-                operacion.setOperacion("sin");
-                añadirNumero();
-                hacerCalculo();
-                break;
-            case R.id.tan:
-                operacion.setOperacion("tan");
-                añadirNumero();
-                hacerCalculo();
+                añadirOperando();
                 break;
 
-            // Igual
-            case R.id.igual:
-                añadirNumero();
-                hacerCalculo();
-                operacion.setOperacion(null);
+            case R.id.cuadrado:
+                operacion.setOperacion("x2");
+                añadirOperando();
+                calcular();
                 break;
 
-            // Borrar
-            case R.id.borrar:
-                operacion.setOperacion(null);
-                numeroUno = "";
-                numeroDos = "";
-                operacion.setNumero1(0);
-                operacion.setNumero2(0);
-                textResultado.setText("");
+            case R.id.btn_cubo:
+                operacion.setOperacion("x3");
+                añadirOperando();
+                calcular();
                 break;
+
+            case R.id.btn_igual:
+                añadirOperando();
+                calcular();
+
+                operacion.setOperacion(null);
+
+
+                break;
+
+
+
+
+
+            case R.id.btn_C:
+                operacion.setOperacion(null);
+                numeroUno=null;
+                numeroDos=null;
+                operacion.setOperando1(0);
+                operacion.setOperando2(0);
+                textoResulatado.setText("");
+                break;
+                
 
         }
+
+
     }
 
-    private void hacerCalculo() {
+    private void calcular() {
 
-        double resultado = 0;
-        operacion.setNumero1(Integer.parseInt(numeroUno));
-        if (numeroDos.length() > 0) {
-            operacion.setNumero2(Integer.parseInt(numeroDos));
-        }
-        switch (operacion.getOperacion()) {
+        int resultado=0;
+        operacion.setOperando1(Integer.parseInt(numeroUno));
+        if (numeroDos.length()>0){operacion.setOperando2(Integer.parseInt(numeroDos));}
+        switch (operacion.getOperacion()){
             case "+":
-                resultado = (operacion.getNumero1() + operacion.getNumero2());
-                textResultado.setText(String.valueOf(resultado));
+               resultado =operacion.getOperando1()+operacion.getOperando2();
+               textoResulatado.setText(String.valueOf(resultado));
                 break;
             case "-":
-                resultado = (operacion.getNumero1() - operacion.getNumero2());
-                textResultado.setText(String.valueOf(resultado));
+                resultado =operacion.getOperando1()-operacion.getOperando2();
+                textoResulatado.setText(String.valueOf(resultado));
                 break;
             case "*":
-                resultado = (operacion.getNumero1() * operacion.getNumero2());
-                textResultado.setText(String.valueOf(resultado));
+                resultado =operacion.getOperando1()*operacion.getOperando2();
+                textoResulatado.setText(String.valueOf(resultado));
                 break;
             case "/":
-                resultado = (operacion.getNumero1() / operacion.getNumero2());
-                textResultado.setText(String.valueOf(resultado));
+                resultado =operacion.getOperando1()/operacion.getOperando2();
+                textoResulatado.setText(String.valueOf(resultado));
                 break;
-            case "X2":
-                resultado = (operacion.getNumero1() * operacion.getNumero1());
-                textResultado.setText(String.valueOf(resultado));
+
+            case "x2":
+                resultado=operacion.getOperando1()* operacion.getOperando1();
+                textoResulatado.setText(String.valueOf(resultado));
                 break;
-            case "X3":
-                resultado = Math.pow(operacion.getNumero1(), 3);
-                textResultado.setText(String.valueOf(resultado));
+            case "x3":
+                resultado= (int) Math.pow(operacion.getOperando1(),3);
+                textoResulatado.setText(String.valueOf(resultado));
                 break;
-            case "sin":
-                resultado = Math.sin(operacion.getNumero1());
-                textResultado.setText(String.valueOf(resultado));
-                break;
-            case "cos":
-                resultado = Math.cos(operacion.getNumero1());
-                textResultado.setText(String.valueOf(resultado));
-                break;
-            case "log":
-                resultado = Math.log10(operacion.getNumero1());
-                textResultado.setText(String.valueOf(resultado));
-                break;
-            case "sqrt":
-                resultado = Math.sqrt(operacion.getNumero1());
-                textResultado.setText(String.valueOf(resultado));
-                break;
-            case "tan":
-                resultado = Math.tan(operacion.getNumero1());
-                textResultado.setText(String.valueOf(resultado));
-                break;
+
         }
-        Log.v("text", String.valueOf(numeroUno));
-        Log.v("text", String.valueOf(numeroDos));
+        Log.v("text",String.valueOf(numeroUno));
+        Log.v("text",String.valueOf(numeroDos));
     }
 
-    private void añadirNumero() {
+    private void añadirOperando() {
 
         if (numeroUno.length() == 0) {
 
-            numeroUno = textResultado.getText().toString();
+            numeroUno = textoResulatado.getText().toString();
             Log.v("text", String.valueOf(numeroUno));
             Log.v("text", String.valueOf(numeroUno.length()));
         } else if (numeroUno.length() > 0 && numeroDos.length() == 0) {
-            numeroDos = textResultado.getText().toString();
+            numeroDos = textoResulatado.getText().toString();
+            //Log.v("text",textoResulatado.getText().toString());
             Log.v("text", String.valueOf(numeroDos));
 
+
         } else if (numeroUno.length() > 0 && numeroDos.length() > 0) {
-            numeroUno = String.valueOf(textResultado.getText());
+            numeroUno = String.valueOf(textoResulatado.getText());
             numeroDos = "";
+
+
         }
-        textResultado.setText("");
-    }
-}
+        textoResulatado.setText("");
+
+
+    }}
